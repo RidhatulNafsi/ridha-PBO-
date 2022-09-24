f312594056887f5deec1f736b926318a84dbb765
RidhatulNafsi <114200748+RidhatulNafsi@users.noreply.github.com>
2022-09-24 16:18:49 +0700

    Tugas Modul 1 dan Modul 4
    RidhatulNafsi_2101092066_MI 2A

---
+++ Tugas 4 PBO/Latihan1Ridha.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.Ridha.p210922;
+
+/**
+ *
+ * @author ridha
+ */
+public class Latihan1Ridha {
+    public static void main( String[] args ){ 
+       int number =10;
+       char letter ='a';
+       boolean result=true;
+       String str="hello";
+               
+               
+       System.out.println("Number = "+number);
+       System.out.println("Letter = "+letter);
+       System.out.println("Result = "+result);
+       System.out.println("Str = "+str);
+    }
+}
---
+++ Tugas 4 PBO/Latihan2Ridha.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.Ridha.p210922;
+
+/**
+ *
+ * @author ridha
+ */
+public class Latihan2Ridha {
+     public static void main(String[] args) {
+       int number1 =10;
+        int number2 =20;
+        int number3 =45;
+        double avarage = 25;
+        
+        System.out.println("Number1 = " + number1);
+        System.out.println("Number2 = " + number2);
+        System.out.println("Number3 = " + number3);
+        System.out.println("Avarage is = " + avarage);
+     } 
+}
