d27b7c243b848e21c837bebeb568e0bedaf96e90
RidhatulNafsi <114200748+RidhatulNafsi@users.noreply.github.com>
2022-11-04 09:53:08 +0700

    pertemuan 120922

---
+++ Ridha120922/DataDiri.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Ridha120922;
+
+/**
+ *
+ * @author ridha
+ */
+public class DataDiri {
+    /*Nama
+      NoBp
+      Prodi
+      Jurusan
+      Alamat
+    
+    */
+    public static void main(String[] args) {
+        System.out.println("Nama\t: Ridhatul Nafsi\n"
+                +"No Bp\t: 2101092066\n"
+                +"Prodi\t: MI\n"
+                +"Jurusan\t: TI\n"
+                +"Alamat\t: Pariaman\n");
+        
+    }
+   }
+    
